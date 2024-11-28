bcc71d3ffc59d3f5ce753ee44ec11ef979e62b76
tomasoignons <emmanuel.b.omont@gmail.com>
2024-11-28 21:00:54 +0100

    added all the pictures

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,8 +1,10 @@
 <template>
-    <nav class="navbar h-16 bg-red-500 flex justify-around items-center">
-        <router-link to="/" class="text-white">Home</router-link>
-        <router-link to="/about" class="text-white">About</router-link>
-        <router-link to="/sub" class="text-white">Sub Page</router-link>
+    <nav class="navbar h-16 bg-red-500 flex justify-center items-center font-bold text-lg">
+        <div class="container flex justify-left items-center max-w-6xl gap-10">
+            <router-link to="/" class="text-white">Home</router-link>
+            <router-link to="/about" class="text-white">About</router-link>
+            <router-link to="/sub" class="text-white">Sub Page</router-link>
+        </div>
     </nav>
   </template>
   
