e35c01abb1d8df5394c9b48d00ec5ee99347b1b4
tomasoignons <emmanuel.b.omont@gmail.com>
2024-12-19 13:35:02 +0100

    refractor a bit the logos, and changed the favicon too

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,9 @@
 <template>
   <nav class="navbar h-16 bg-gray-100 flex justify-center items-center font-bold text-lg">
     <div class="container flex justify-left items-center max-w-6xl gap-10">
+      <router-link to="/" class="text-black" active-class="text-red-600">
+        <img src="/svg/logo/EPFLMUN.svg" alt="Logo" class="h-10" />
+      </router-link>
       <router-link to="/" class="text-black" active-class="text-red-600">Home</router-link>
       <router-link to="/about" class="text-black" active-class="text-red-600">About</router-link>
       <router-link to="/events" class="text-black" active-class="text-red-600">Events</router-link>
